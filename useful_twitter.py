a4018a3762a92e48ebde80ca3bf1484427df70a9
saurabh <saurabh.chaturvedi63@gmail.com>
2017-02-01 04:09:11 +0530

    Added Molly's offensive compilation for filtering out offensive tweets

--- useful_twitter.py
+++ useful_twitter.py
@@ -1,5 +1,22 @@
 from twitter import Twitter, OAuth, TwitterHTTPError, TwitterStream
-import webbrowser, time, random
+import webbrowser, time, random, re
+
+offensive = re.compile(
+    r"\b(deaths?|dead(ly)?|die(s|d)?|hurts?|(sex(ual(ly)?)?|child)[ -]?(abused?|trafficking|"
+    r"assault(ed|s)?)|injur(e|i?es|ed|y)|kill(ing|ed|er|s)?s?|wound(ing|ed|s)?|fatal(ly|ity)?|"
+    r"shoo?t(s|ing|er)?s?|crash(es|ed|ing)?|attack(s|ers?|ing|ed)?|murder(s|er|ed|ing)?s?|"
+    r"hostages?|(gang)?rap(e|es|ed|ist|ists|ing)|assault(s|ed)?|pile-?ups?|massacre(s|d)?|"
+    r"assassinate(d|s)?|sla(y|in|yed|ys|ying|yings)|victims?|tortur(e|ed|ing|es)|"
+    r"execut(e|ion|ed|ioner)s?|gun(man|men|ned)|suicid(e|al|es)|bomb(s|ed|ing|ings|er|ers)?|"
+    r"mass[- ]?graves?|bloodshed|state[- ]?of[- ]?emergency|al[- ]?Qaeda|blasts?|violen(t|ce)|"
+    r"lethal|cancer(ous)?|stab(bed|bing|ber)?s?|casualt(y|ies)|sla(y|ying|yer|in)|"
+    r"drown(s|ing|ed|ings)?|bod(y|ies)|kidnap(s|ped|per|pers|ping|pings)?|rampage|beat(ings?|en)|"
+    r"terminal(ly)?|abduct(s|ed|ion)?s?|missing|behead(s|ed|ings?)?|homicid(e|es|al)|"
+    r"burn(s|ed|ing)? alive|decapitated?s?|jihadi?s?t?|hang(ed|ing|s)?|funerals?|traged(y|ies)|"
+    r"autops(y|ies)|child sex|sob(s|bing|bed)?|pa?edophil(e|es|ia)|9(/|-)11|Sept(ember|\.)? 11|"
+    r"genocide)\W?\b",
+    flags=re.IGNORECASE) #Copyright (c) 2013-2016 Molly White
+    #Above offensive compilation is not my stuff
 
 # Variables that contain the user credentials to access Twitter API
 
@@ -114,10 +131,14 @@ while 1:
     tags = ["heroku", "angularjs", "nodejs", "java", "javascript", "python"]
     tweets = t.search.tweets(q=random.choice(tags), count=199)["statuses"]
     for tweet in tweets:
-        retweet(tweet)
-        fav_tweet(tweet)
         try:
-            t.friendships.create(_id=tweet["user"]["id"])
+            if re.search(offensive, tweet["text"]) == None:
+                retweet(tweet)
+                fav_tweet(tweet)
+                try:
+                    t.friendships.create(_id=tweet["user"]["id"])
+                except:
+                    pass
         except:
             pass
     #search_and_fav("python programming", 199)
