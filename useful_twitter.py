8bc1d00abba8612c8b880cabcaa12aa43b5f5132
Saurabh S. Chaturvedi <saurabh.chaturvedi63@gmail.com>
2017-09-09 15:01:23 +0530

    Reduce unfollow trigger

--- useful_twitter.py
+++ useful_twitter.py
@@ -271,7 +271,7 @@ class AccountThread(threading.Thread):
             to follow some interesting people manually even after a
             bot crash.
             """
-            if len(fr) > 4990:
+            if len(fr) > 4200:
                 # Perhaps the upper limit for mass unfollow is 1000 a day.
                 for i in range(500):
                     unfollow(fr.pop())
